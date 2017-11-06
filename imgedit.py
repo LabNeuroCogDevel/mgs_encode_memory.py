587d4dea63a98172e7fb6db71d4261c87eb1a5f9
WillForan <willforan@gmail.com>
2017-11-06 16:08:18 -0500

    fix no default "circlesize"; track selected images

--- imgedit.py
+++ imgedit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # https://stackoverflow.com/questions/890051/how-do-i-generate-circular-thumbnails-with-pil
 import PIL, PIL.ImageDraw, PIL.ImageOps
-import os,os.path, glob
+import os,os.path, glob, re
 
-def make_circle(imfile):
+def make_circle(imfile,circlesize=[(0,0),(225,225)]):
   # read in image
   im = PIL.Image.open(imfile)
   # we want all images to be the same size
@@ -26,11 +26,19 @@ def make_circle(imfile):
   #im.save('test.png')
   #im.show()
 
-if not os.path.exists('img_circle'): os.mkdir('img_circle')
-for imgfile in glob.glob('img/*png'):
-    im_alpha= make_circle(imgfile)
-    outname=os.path.basename(imgfile)
-    outfile=os.path.join('img_circle',outname)
-    print(imgfile)
-    im_alpha.save(outfile)
+if __name__ == "__main__":
+ import sys
+ if not os.path.exists('img_circle'): os.mkdir('img_circle')
+
+ if len(sys.argv) != 2:
+     patt='img/*png'
+ else:
+     patt=sys.argv[1]
+     
+ for imgfile in glob.glob(patt):
+     im_alpha= make_circle(imgfile)
+     outname=re.sub('.jpe?g$','.png',os.path.basename(imgfile))
+     outfile=os.path.join('img_circle',outname)
+     print(imgfile)
+     im_alpha.save(outfile)
      
