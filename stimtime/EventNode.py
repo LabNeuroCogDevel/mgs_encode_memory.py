6045449998c759e849a85df2203bed19a1da1abf
Will Foran <wforan1@andrew.cmu.edu>
2017-11-13 21:35:45 -0500

    working for "<300/30> vgs=[1.5]( Left, Right * Indoor,Outdoor,None ); dly=[5x 2, 4x 3, 1x 4]; mgs=[1.5]"

--- stimtime/EventNode.py
+++ stimtime/EventNode.py
@@ -1,4 +1,4 @@
-import anytree, functools
+import anytree, functools, math, random
 
 def unlist_grammar(e):
     final=[]
@@ -14,35 +14,20 @@ def unlist_grammar(e):
 # what we need to store for each event entering our event tree
 class EventNode(anytree.NodeMixin):
 
-    # TODO: handle distibutions
-    def parse_dur(self,dur):
-        if type(dur) in [float, int, type(None)]:
-          self.dur=dur
-          return
-        elif dur['dur']:
-          self.dur=dur['dur']
-        elif dur['min']:
-          self.dur=[ float(dur['min']),float(dur['max']) ]
-          # todo distribute
-        elif dur['steps']:
-          steps=unlist_grammar( dur['steps'] )
-          # todo, distribute
-          self.dur= functools.reduce(lambda x,y: x+y, [  [float(x['num'])]*int(x['freq']) for x in steps] ) 
-        
 
-    def __init__(self,name,dur,parent=None,nrep=1,writeout=True,isfree=False,extra=None):
+    def __init__(self,name,dur,parent=None,nrep=1,writeout=True,extra=None):
         if(nrep == None): nrep=1
         self.parent=parent
         self.name=name
-        self.parse_dur(dur)
+        self.dur = dur #parse_dur(dur)
         self.nrep=nrep
         self.writeout=writeout
-        self.isfree=isfree
         self.last=False
+        #self.finalized = False
         self.extra=extra
 
     def __repr__(self):
-        return("%s=[%s] x %s (%dd %dc)"%(self.name,self.dur,self.nrep,len(self.path),len(self.children)) )
+        return("%s=%s (%dd|%dc)"%(self.name,self.nrep,len(self.path),len(self.children)) )
 
     # count up and make list of parents to traverse
     def set_last(self):
@@ -57,18 +42,76 @@ class EventNode(anytree.NodeMixin):
             p=p.parent
         return(self)
 
-    def set_free(self,ntrials):
-        self.isfree=isfree
-        self.nrep=0 # don't count in tree
-        self.need_total=ntrials
-        # TODO: better dur handling
-        self.origdur= [self.dur] * self.need_total
-        random.shuffle(self.origdur)
-        return(self)
+    # how many times is this node hit on it's children branches
+    # N.B. still need to combine across branches
+    def count_reps(node):
+        totalreps=0
+        children=node.children
+        for c in children: totalreps+= c.count_reps()
+        if node.nrep: 
+            if len(children)==0: totalreps=node.nrep
+            else: totalreps*=node.nrep 
+        #print("%d for all %d children of %s"%(totalreps,len(children),node))
+        node.total_reps=totalreps
+        return(totalreps)
+        
 
+    # TODO: handle distibutions
+    def parse_dur(self,nperms):
+        ## how many durs do we need?
+        # -- should probably stop if do not have total_reps
+        nsamples= getattr(self, "master_total_reps", \
+                           getattr(self,"total_reps",\
+                                    getattr(self,"nrep",0) ) )
+
+        nsamples*=nperms
+
+        if type(self.dur) in [float, int, type(None)]:
+          dur=[self.dur] * nsamples
+        elif self.dur['dur']:
+          dur= [ float(self.dur['dur']) ] * int(nsamples)
+
+        elif self.dur['min']:
+          # todo distribute for others (just uniform now)
+          a=float(self.dur['min'])
+          b=float(self.dur['max'])
+          intv=(b-a)/(nsamples-1)
+          dur=[ a+i*intv for i in range(nsamples)  ]
+
+        elif self.dur['steps']:
+          steps=unlist_grammar( self.dur['steps'] )
+          # todo, distribute
+          dur = functools.reduce(lambda x,y: x+y, [  [float(x['num'])]*int(x['freq']) for x in steps] ) 
+          ndur=len(dur)
+          if ndur > nsamples:
+              print("WARNING: %s: you have more durations (%d) than trials (%d). randomly truncating"%(self.name,ndur,nsamples))
+          # fit what was given into what we have
+          if ndur < nsamples:
+              times_more = math.floor(nsamples/ndur)
+              add_more   = ndur%nsamples
+              origdur = dur
+              dur = origdur + (origdur * (times_more-1))
+              random.shuffle(origdur)
+              if add_more > 0:
+                 print("WARNING: %s: num durations given (%d) doesn't fit with number of events (%d) equally. randomly picking"%(self.name,ndur,nsamples))
+                 dur= dur + origdur[0:add_more]
+              else:
+                 dur= dur + origdur
+            
+        random.shuffle(dur)
+        dur=dur[0:nsamples]
+        self.dur_dist = dur
+        self.dur_dist_avg = functools.reduce(lambda x,y:x+y,dur)/len(dur)
+        return(dur)
             
     def next_dur(self):
-        if not self.isfree:
-            return(self.dur)
+        if len(self.dur_dist)==0:
+            print("WARNING: %s: no more durations to pick from. giving 0"%self.name)
+            return(0)
         else:
-            return(self.orgdur.pop)
+            #if not self.picked: self.picked=0
+            #self.picked+=1
+            return(self.dur_dist.pop())
+
+
+
--- stimtime/mktime.py
+++ stimtime/mktime.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import random, anytree, copy
+import random, anytree, copy, pprint, itertools, functools
 import EventNode, EventGrammar
 
 
-def parse_events(ast):
-    if ast == None: return
-    events=EventGrammar.unlist_grammar(ast['allevents'])
+def parse_events(astobj):
+    if astobj == None: return
+    events=EventGrammar.unlist_grammar(astobj['allevents'])
     # TODO: recursively expand subevents that are events in full
     return(events)
 
 # subevent list is an elment of 'eventtypes'
 # this builds a tree of them
 # a=mkChild(EventNode.EventNode('root',dur=0),events[0]['eventtypes'])
+    print("\n#### events")
 def mkChild(parents,elist):
     subevent_list=EventGrammar.unlist_grammar(copy.deepcopy(elist)) #tmp copy because we're poping off it
     if type(parents) != list:
@@ -84,13 +85,143 @@ def events_to_tree(events):
 def free_nodes(root):
     pass
 
-def find_root(leaf):
-    root = leaf
-    while root.parent: root=root.parent
-    return(root)
+# dont need this
+def find_terminal_leaves(node):
+   ends=[]
+   if len(node.children)==0 or node.children == None:
+       print(node)
+       return([node])
+   for c in node.children:
+      ends += find_terminal_leaves(c) 
+   return(ends)
+
+
+## create a master node used calculate delays
+# each node in the tree with the same name points to the same master node
+def create_master_refs(root):
+    # root requires no calculation -- there is only one (right!?)
+    uniquenode = lambda n: '%s'%n.name # '%s %s'%(n.name,n.dur)
+    masternodes = {}
+
+    for n in (root, *root.descendants):
+        nname = uniquenode(n)
+        if not masternodes.get(nname):
+          n.master_total_reps = 0
+          masternodes[nname] = n
+        n.master_node=masternodes.get(nname)
+        n.master_node.master_total_reps+=n.total_reps
+    return([ x for x in masternodes.values() ] )
     
-def write_trials(tree,settings):
-   pass 
+def write_trials(last_leaves,settings):
+    # settings
+    ntrials=int( settings['ntrial'] )
+
+    # how many times do we go down the tree?
+    nperms=0
+    for l in last_leaves:
+        l.set_last()
+        nperms+=l.need_total
+
+    n_rep_branches=ntrials/nperms
+
+    # check sanity
+    if n_rep_branches < 1:
+        print('WARNING: your expreiment has too few trials (%d) to accomidate all branches(%d)'%(ntrials,nperms))
+        n_rep_branches=1
+    elif n_rep_branches != int(n_rep_branches):
+        print('WARNING: your expreiment will not be balanced\n\t' +
+              '%d trials / %d event branches is not a whole number (%f);'%(ntrials,nperms,n_rep_branches, ))
+              #'maybe try %f trials '%(ntrials,nperms,n_rep_branches, (int(n_rep_branches)*nperms) ))
+
+    n_rep_branches=int(n_rep_branches)
+
+    # each node should have the nrep sum of its children (x nreps of that node)
+    # node.totalreps
+    root=last_leaves[0].root
+
+    # get how many time each node in the tree will be seen
+    root.count_reps() 
+    # different branches have nodes with the same name
+    # link them all to one node so we can draw duration times from that one
+    unique_nodes = create_master_refs(root)
+
+    # set up delay distributions
+    for u in unique_nodes:
+        u.parse_dur(n_rep_branches)
+
+    # todo: add min iti from settings
+    min_iti=1.5
+
+    # tree to list
+    triallist=[]
+    for l in last_leaves:
+        branch = l.parents
+        fname=[]
+        for i in range(l.nrep):
+            thistrial=[]
+            for n in branch:
+                n=n.master_node
+                if n.dur != 0:
+                    if fname:
+                        thistrial.append( {'fname':fname, 'dur': dur} )
+                    fname=[]
+                    dur=0
+                fname.append(n.name)
+                dur+=n.next_dur()
+            if fname: thistrial.append( {'fname':fname, 'dur': dur} )
+            thistrial.append( {'fname': None, 'dur': min_iti} )
+
+            for i in range(l.total_reps * n_rep_branches): triallist.append(thistrial)
+
+                
+    #[ [{'fname': '', 'dur': '' }, ...], ... ]
+    all_durs = [ o['dur'] for x in triallist for o in x ] 
+    total_dur=functools.reduce(lambda x,y:x+y, all_durs)
+    # TODO: add min ITI
+
+    rundur=float(settings['rundur'])
+    if total_dur > rundur:
+        print("ERROR: total event time (%.2f) is greater than run time (%.2f)!"%(total_dur,rundur))
+        rundur = total_dur # TODO, maybe die instead?
+        
+
+    # TODO:
+    # - use specified granularity 
+    # - remove start/end times from count
+    granularity=.01
+    n_iti = int( (rundur - total_dur ) / granularity )
+
+    triallist += [ [ {'fname': None, 'dur': granularity} ] ] * n_iti
+
+    random.shuffle(triallist)
+    total_time=0
+    for tt in triallist:
+        for  t in tt:
+            if t['fname']:
+                outname="_".join(t['fname'])
+                print('%s: %f (%f)'%(outname,total_time,t['dur']))
+            total_time+=t['dur']
+
+    print('stop')
+           
+    
+
+    # calc final dur of leaves
+
+    ## sequence: [{name:,dur:, treepath: }] of iti/rest + events (with min iti added)
+    # todo tr lock by setting gran to tr and rounding durations
+    # can we divide perms evenly into trials
+
+    # for each last_leaf, 
+    #  for each path
+    #    use node to lookup masternodes, call get_dur
+    #    build sequence, add min iti to seq dur
+    #    track total time 
+ 
+    # fill the renaming time with {iti: , dur: gran}
+    
+    # dont do it this way
+    #for t in range(1,ast['settings']['ntrial']):
 
 
 
@@ -105,36 +236,29 @@ if __name__ == '__main__':
         #expstr='<300/32> vgs=[1.5]( (Left,Right) * (Near, Far &) ); dly=[2,4,6g]; mgs=[1.5]'
         expstr='<1/1>vgs=[1.5]( Left,Right * Near,Far ~); dly=[4x 2,3x 4,2x 6];mgs=[1.5]'
 
-    ast = EventGrammar.parse(expstr)
+    astobj = EventGrammar.parse(expstr)
     print("have")
-    pprint.pprint(ast,indent=2,width=20)
-    events=parse_events(ast)
+    pprint.pprint(astobj,indent=2,width=20)
+    events=parse_events(astobj)
 
-    print("\n#### events")
     pprint.pprint(events,indent=2,width=20)
     print("\n#### calc tree")
 
     # build a tree from events
     last_leaves = events_to_tree(events)
+    # kludgyhack: event_to_tree does some weird things. rather than fix it, lets work around it
+    root = last_leaves[0].root
+    #find_terminal_leaves = 
 
     # set the terminal leaves, and count number of permutations
-    nperms=0
-    for l in last_leaves:
-        l.set_last()
-        nperms+=l.need_total
 
-    # can we divide perms evenly into trials
-    #settings
-    
-    # find root
-    root=find_root(last_leaves[0])
+
 
     ## print some more stuff
     print("\n#### tree")
     print(anytree.RenderTree(root))
     print("\n#### last leaves")
     print(last_leaves) 
-    print(nperms)
-    write_trials(last_leaves,ast['settings'])
-#for t in range(1,ast['settings']['ntrial']):
+    write_trials(last_leaves,astobj['settings'])
+
         
