04dda602dc545c14b456ba428c14a29b61164554
crkolimago <crkolimago@gmail.com>
2019-03-18 11:39:04 -0400

    Plotted geom smooth lines for quality assurance.

--- analysis/fixations.R
+++ analysis/fixations.R
@@ -12,7 +12,7 @@ GetSpread <- function(fname) {
   
   # Average of last 90 data points for each id->side->event->trial
   data.mt90 <- data %>%
-    group_by(trial, event, side, ld8) %>%
+    group_by(trial, event, side, ld8, runno) %>%
     summarise(mt90=mean(tail(x_gaze,90))) %>%
     ungroup() %>%
     mutate(side=ifelse(event=="iti",lag(side),side)) %>%
@@ -21,12 +21,6 @@ GetSpread <- function(fname) {
   # Near/Right = 1; Near/Left = -1
   data.mt90$side_num <- grepl("Right",data.mt90$side) %>%
     ifelse(1,-1)
-  
-  # Run and filename processing
-  file_base <- basename(fname)
-  reg_start <- regexpr("run.", file_base, TRUE)
-  reg_end <- attr(reg_start, "match.length")-1
-  run_num <- substr(file_base, reg_start, reg_start+reg_end)
   
   # Trial order:
   #  iti -> cue -> img -> isi -> mgs
@@ -37,26 +31,63 @@ GetSpread <- function(fname) {
   #  2 = side_num != ic (=/!= mi)
   
   data.sides <- data.mt90 %>%
-    mutate(ic=(img-cue)/abs(img-cue), mi=(mgs-isi)/abs(mgs-isi), score=ifelse(side_num==ic,ifelse(ic==mi,0,1),2)) %>%
-    mutate(file=file_base, run=run_num)
+    mutate(ic=(img-cue)/abs(img-cue), mi=(mgs-isi)/abs(mgs-isi), score=ifelse(side_num==ic,ifelse(ic==mi,0,1),2))
   
-  return(data.sides)
+  ## DATA QUALITY ##
+  
+  trial_scores <- data.sides %>%
+    select(c(trial, score, ld8, runno)) %>%
+    merge(data, by=c("trial","ld8", "runno"))
+  
+  return(trial_scores)
+  #return(data.sides)
 }
 
 # Apply scoring to files[1:last_file] times
 doAll <- function() {
-  last_file <- 24
+  last_file <- 5
+  #scored.out <- lapply(files[1:last_file], function(fname) tryCatch(GetSpread(fname), error=function(x) NULL)) %>% bind_rows()
   scored.out <- lapply(files[1:last_file], function(fname) tryCatch(GetSpread(fname), error=function(x) NULL)) %>% bind_rows()
 }
 
 # Retrieve large row-bound dataframe
 scored.out <- suppressWarnings(doAll())
 
+mgs_data <- scored.out %>% 
+  #select(c(ld8, runno, trial, score, event, totaltime)) %>%
+  filter(event=="mgs")
+
+ trial_start_times <- mgs_data %>%
+  group_by(ld8, runno, trial) %>%
+  summarise(mtime=min(totaltime))
+
+ run_start_times <- mgs_data %>%
+   group_by(ld8, runno) %>%
+   summarise(mtime=min(totaltime))
+ 
+ 
+ctimes <- merge(mgs_data, trial_start_times)
+
+ctimes$adjusted_time <- ctimes$totaltime-(ctimes$mtime)
+
+ctimes<-ctimes %>% select(-mtime)
+
+ss<- ctimes %>%
+  arrange(ld8, runno, trial) %>%
+  select(trial, ld8, runno, score, x_gaze, event, side, adjusted_time) %>%
+  filter(ld8==unique(ctimes$ld8)[1])
+
+#ctimes %>% ggplot()+aes(x=adjusted_time, y=x_gaze)+geom_point(aes(color=runno))+geom_smooth()+facet_wrap(runno~trial)
+#ctimes %>% ggplot()+ylim(-2,2)+aes(x=adjusted_time, y=x_gaze)+geom_point(aes(color=trial))+geom_smooth(aes(color=trial))+facet_wrap(ld8~runno)
+
+ss %>%
+  ggplot()+aes(x=adjusted_time, y=x_gaze)+geom_smooth(aes(color=side, group=paste(trial,runno)), se=FALSE)+ylim(-2,2)+xlim(0,1.0)+facet_wrap(~side)
+
+
+
 # Box plot
 #  x = ld8
 #  y = score
 #  Jitter points shown (colored by run)
-gg_box <- scored.out %>%
-  ggplot()+aes(x=ld8,y=score)+geom_boxplot()+geom_jitter(aes(color=as.factor(run)))
-
-
+#gg_box <- scored.out %>%
+#  ggplot()+aes(x=ld8,y=score)+geom_boxplot()+geom_jitter(aes(color=as.factor(run)))
